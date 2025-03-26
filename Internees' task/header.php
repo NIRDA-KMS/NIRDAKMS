f77863663755cb646c61f88ec118d454a6ef4d5d
valens <ukwitegetsev9@gamail.com>
2025-03-26 12:15:07 +0200

    link of stylesheet

--- Internees' task/header.php
+++ Internees' task/header.php
@@ -8,7 +8,7 @@
 <title>NIRDA Knowledge Management System</title>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="../header.css">
 </head>
 <body>
     <nav class="navbar">
